b229c2ab15161984207088e6074191a2e26f5428
GitShivSagar <shivsagarstd77@gmail.com>
2026-05-29 18:31:56 +0530

    update

--- BackEnd/dbConnect/dbConnect.js
+++ BackEnd/dbConnect/dbConnect.js
@@ -6,6 +6,7 @@ const dbConnect=async(DB_URL,DB_NAME)=>{
         console.log("Database Connected Successfully")
     } catch (error) {
         console.log("Database not Connected")
+        console.log(error)
     }
 }
 
